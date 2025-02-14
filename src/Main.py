402927ee653e9615f355051af8eb65222a33e1e7
Miquéias Ferreira <queias2909@gmail.com>
2025-02-13 21:40:43 -0300

    background update

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,9 @@
 import pgzrun as pg
-
+from random import randint
 
 # === Setting the width and height of the window ===
-WIDTH = 800
-HEIGHT = 300
+WIDTH = 576
+HEIGHT = 324
 
 # === Characters classes ===
 class Player:
@@ -12,9 +12,10 @@ class Player:
         self.velocity = 3
         self.direction = "right"
         self.spriteSize = (60, 73)
-        self.initialPos = (30, HEIGHT-self.spriteSize[1])
+        self.initialPos = (int(WIDTH/2 - self.spriteSize[0]/2), int(HEIGHT - self.spriteSize[1]*1.5))
         self.animation_speed_to_idle = 8
         self.animation_speed_to_run = 4
+        self.isWalking = False
 
         # load sprites
         self.runImages = {
@@ -34,7 +35,7 @@ class Player:
 
         # Setting Actor object
         self.player = Actor(self.currentImages[self.current_frame], self.spriteSize)
-        self.player.pos = self.initialPos
+        self.player.topleft = self.initialPos
 
     def update(self):
         self.frame_count += 1
@@ -59,16 +60,138 @@ class Player:
             self.player.left -= self.velocity
     
     def toIdle(self):
+        self.isWalking = False
         self.animation_speed = self.animation_speed_to_idle
         self.currentImages = self.idleImages[self.direction]
 
     def toRun(self):
+        self.isWalking = True
         self.animation_speed = self.animation_speed_to_run
         self.currentImages = self.runImages[self.direction]
 
+    def goingToLeft(self):
+        if(self.player.left < 10 and self.isWalking and self.direction == "left"):
+            return True
+        return False
+    
+    def goingToRight(self):
+        if(self.player.right > WIDTH - 10 and self.isWalking and self.direction == "right"):
+            return True
+        return False
+
+class Cloud:
+    def __init__(self):
+        self.x = WIDTH
+        self.y = randint(int(HEIGHT*0.1), int(HEIGHT*0.6))
+        self.cloudImage = "cloud"
+        self.cloudVelocity = 1
+        self.canBeRemoved = False
+
+        self.cloud = Actor(self.cloudImage, topleft=(self.x, self.y))
+    
+    def update(self):
+        if(self.cloud.right >= 0):
+            self.cloud.left -= self.cloudVelocity
+        else:
+            self.canBeRemoved = True
+    def draw(self):
+        self.cloud.draw()
+
+class Ground:
+    def __init__(self, groundImage:str, firstGround:bool, posx:int=0):
+        if(firstGround):
+            self.x = 0
+        else:
+            self.x = posx
+
+        self.groundValocity = 2
+        self.canBeRemoved = False
+        self.direction = "stop"
+        self.ground = Actor(groundImage, topleft=(self.x, 0))
+    
+    def LeftOutTheScreen(self):
+        if(self.ground.right < 0 or self.ground.left > WIDTH):
+            self.canBeRemoved = True
+
+    def moveRight(self):
+        self.ground.left += self.groundValocity
+    def moveLeft(self):
+        self.ground.left -= self.groundValocity
+
+    def draw(self):
+        self.ground.draw()
+
+class Scene:
+    def __init__(self, player:Player):
+        self.sky = "sky"
+        self.clouds = []
+        self.cloudTick = 0
+        self.player = player
+
+        self.ground_width = 576 
+        self.grounds = [Ground("ground", True), Ground("ground", False, self.ground_width)]
+
+        self.mountains = "mountains"
+        self.layer = "layer"
+
+    def groundManage(self):
+        for ground in self.grounds:
+            ground.LeftOutTheScreen()
+            ground.draw()
+
+        # Remover os blocos que saíram da tela
+        self.grounds = [ground for ground in self.grounds if not ground.canBeRemoved]
+
+        self.moveGrounds()
+        self.createGround()
+
+    def moveGrounds(self):
+        if self.player.goingToLeft():
+            for ground in self.grounds:
+                ground.moveRight()
+        if self.player.goingToRight():
+            for ground in self.grounds:
+                ground.moveLeft()
+
+    def createGround(self):
+        # Pega a posição do último e do primeiro bloco de chão existentes
+        last_ground = self.grounds[-1]
+        first_ground = self.grounds[0]
+
+        # Adicionar um novo bloco à direita se necessário
+        if last_ground.ground.right < WIDTH:
+            new_ground = Ground("ground", False, last_ground.ground.right)
+            self.grounds.append(new_ground)
+
+        # Adicionar um novo bloco à esquerda se necessário
+        if first_ground.ground.left > 0:
+            new_ground = Ground("ground", False, first_ground.ground.left - self.ground_width)
+            self.grounds.insert(0, new_ground)
+
+    def cloudManage(self):
+        self.cloudTick += 1
+        # Draw and remove clouds
+        for cloud in self.clouds:
+            cloud.draw()
+            cloud.update()
+            if(cloud.canBeRemoved):
+                self.clouds.remove(cloud)
+        self.cloudSpaw()
+
+    def cloudSpaw(self):
+        if(self.cloudTick > 120):
+            self.cloudTick = 0
+            self.clouds.append(Cloud()) if randint(0, 1) == 1 and len(self.clouds) < 5 else ...
+            
+    def showScene(self):
+        screen.blit(self.sky, (0, 0))
+        self.cloudManage()
+        screen.blit(self.mountains, (0,0))
+        screen.blit(self.layer, (0, 0))
+        self.groundManage()
+
 player = Player()
-
-
+scene = Scene(player)
 
 def update():
     #Player animation
@@ -82,7 +205,7 @@ def update():
 
 def draw():
     screen.clear()
-    screen.fill("white")
+    scene.showScene()
     player.draw()
 
 def on_key_up(key):
